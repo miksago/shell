9156bc8e829d3dbfd91378e4480c3ad6a004f662
Micheil Smith <micheil@brandedcode.com>
2013-11-28 10:01:56 +0000

    Try switching to reconnect instead of shoe

--- browser/main.js
+++ browser/main.js
@@ -1,16 +1,21 @@
-var shoe = require('shoe');
-var term = require('./index')(80, 25);
+var reconnect = require('reconnect/shoe');
+var terminal = require('./index');
 
 var container = document.getElementById("console");
 
-var stream = shoe('/sock');
+// var stream = shoe('/sock');
 // stream.on('end', function () {
 //     window.close();
 // });
 
-term.pipe(stream).pipe(term);
+var term = terminal(80, 25);
+reconnect(function (stream) {
+  term.pipe(stream).pipe(term);
+}).connect('/sock');
+
 term.appendTo(container);
 term.listenTo(document);
+
 
 function resize () {
   term.resize(container.offsetWidth, container.offsetHeight);
