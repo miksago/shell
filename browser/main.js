76f56e535e36ee24c1848413d5a952f2dd4594be
Micheil Smith <micheil@brandedcode.com>
2013-11-28 05:29:06 +0000

    First working version: 0.0.1

--- browser/main.js
+++ browser/main.js
@@ -1,10 +1,11 @@
 var shoe = require('shoe');
-var term = require('../')(80, 25);
+var term = require('./index')(80, 25);
 
 var stream = shoe('/sock');
 stream.on('end', function () {
     window.close();
 });
+
 term.pipe(stream).pipe(term);
 term.appendTo(document.body);
 term.listenTo(document);
---
+++ server.js
@@ -0,0 +1,162 @@
+#!/usr/bin/env node
+var url = require("url");
+var http = require("http");
+
+var shoe = require('shoe');
+var shux = require('shux')();
+var path = require('path');
+
+var express = require("express");
+var uuid = require("uuid");
+var browserify = require('browserify-middleware');
+
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
+
+var shellConnections = {};
+
+function hasConnection(id){
+    return shellConnections.hasOwnProperty(id);
+}
+
+function getConnection(id){
+    return shellConnections[id];
+}
+
+
+var indexFile = path.join(__dirname, "static", "index.html");
+
+var app = express();
+
+app.use(express.logger());
+
+app.use(app.router);
+
+app.get("/", function(req, res){
+    res.redirect("/session/" + uuid());
+});
+
+app.get("/session/:id", function(req, res){
+    // If we already have that shell, redirect to a new one:
+    if(hasConnection(req.params.id)){
+        return res.redirect("/");
+    }
+    // Else, server up the console:
+    res.status(200);
+    res.sendfile(indexFile);
+});
+
+app.get("/viewer/:id", function(req, res){
+    // If that shell doesn't exist, error:
+    if(!hasConnection(req.params.id)){
+        res.status(404);
+        res.end("Not found :(");
+        return;
+    }
+
+    res.status(200);
+    res.sendfile(indexFile);
+});
+
+app.get('/bundle.js', browserify(path.join(__dirname, "browser", "main.js")));
+
+
+// Http:
+var server = http.createServer();
+
+var sock, sockHandler;
+
+var shellConnections = {};
+
+var PATH_REGEX = /^\/([^\/]+)\/([^\/]+)/;
+
+sock = shoe(function (stream) {
+    var pathname = url.parse(stream.headers.referer).pathname;
+    var matches = pathname.match(PATH_REGEX);
+
+    if(matches.length < 3){
+        stream.end();
+    }
+
+    var shell;
+    var shellType = stream.shellType = matches[1];
+    var shellId = stream.shellId = matches[2];
+
+    if(shellType === "session" && !hasConnection(shellId)){
+        shell = shux.createShell({ command: process.env.SHELL || "bash", id: shellId });
+
+        shellConnections[shellId] = {
+            shell: shell,
+            viewers: []
+        };
+
+        // Clean up:
+        stream.on("end", function(){
+            var connection = getConnection(shellId);
+
+            if(connection && connection.viewers.length){
+                connection.viewers.forEach(function(viewer){
+                    viewer.stream.end();
+                });
+            }
+
+            delete connection;
+            delete shellConnections[shellId];
+        });
+
+        stream.pipe(shell).pipe(stream);
+
+        return;
+    }
+
+    if(shellType === "viewer" && hasConnection(shellId)){
+        var connection = getConnection(shellId);
+        var viewer = {
+            stream: stream
+        };
+        
+        connection.viewers.push(viewer);
+
+        stream.on("end", function(){
+            var connection = getConnection(shellId);
+            if(connection){
+                var viewerIdx = connection.viewers.indexOf(viewer);
+                if(!!~viewerIdx){
+                    connection.viewers.remove(viewerIdx);
+                }
+            }
+        });
+
+        shell = shux.attach(connection.shell.id);
+
+        stream.pipe(shell).pipe(stream);
+
+        return;
+    }
+
+    if(shellType === "session"){
+        stream.write("This shell somehow already exists. Please reload");
+    }else{
+        stream.write("No such shell");
+    }
+
+    stream.end();
+});
+
+var sockHandler = sock.listener({
+    prefix: '/sock'
+}).getHandler();
+
+server.on('request', function(req, res){
+    if(!sockHandler(req, res)){
+        return app(req, res);
+    }
+});
+
+
+server.listen(parseInt(process.env.PORT, 10), process.env.HOST);
+
