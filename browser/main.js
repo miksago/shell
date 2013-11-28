d0259c7858fcdc74c097377692caa863d02df249
Micheil Smith <micheil@brandedcode.com>
2013-11-28 06:52:37 +0000

    Improvements to screen size

--- browser/main.js
+++ browser/main.js
@@ -1,17 +1,19 @@
 var shoe = require('shoe');
 var term = require('./index')(80, 25);
 
+var container = document.getElementById("console");
+
 var stream = shoe('/sock');
 stream.on('end', function () {
     window.close();
 });
 
 term.pipe(stream).pipe(term);
-term.appendTo(document.body);
+term.appendTo(container);
 term.listenTo(document);
 
 function resize () {
-    term.resize(window.innerWidth - 5, window.innerHeight - 2);
+  term.resize(container.offsetWidth, container.offsetHeight);
 }
 resize();
 window.addEventListener('resize', resize);
